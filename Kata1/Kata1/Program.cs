f267115441a67134daf2c575eb879ba5a6ab05d5
Daniel Tieben <Dannbott101@gmail.com>
2025-10-29 12:45:40 -0600

    Kata 1 finished

---
+++ Kata1/Kata1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Kata1;
+
+class Program
+{
+    static void Main()
+    {
+        var elements = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+        
+        Manager.Instance.StartTheFunny(elements);
+    }
+}
