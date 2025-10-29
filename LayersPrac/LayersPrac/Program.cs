e44ae581d3c0e22a7b0f6e0b651851d706ba3833
Daniel Tieben <Dannbott101@gmail.com>
2025-10-29 11:34:03 -0600

    Layers prac

---
+++ LayersPrac/LayersPrac/Program.cs
@@ -0,0 +1,9 @@
+﻿class LayersPrac
+{
+    public static void Main()
+    {
+        var readFromFile = File.ReadAllText("C:\\Users\\Dtieben\\Documents\\GitHub\\Software-Design-PrincipalsCSC360\\LayersPrac\\LayersPrac\\_README.txt");
+        
+        Console.WriteLine(readFromFile);
+    }
+}
---
+++ LayersPrac/LayersPrac/_README.txt
@@ -0,0 +1,1 @@
+﻿Think Fast Chuckle-Nuts
