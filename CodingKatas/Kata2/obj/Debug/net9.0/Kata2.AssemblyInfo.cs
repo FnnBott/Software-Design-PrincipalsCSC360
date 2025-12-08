6c65d9bc6e3e4d1a1fac56f8ab09db485383d82f
Daniel Tieben <Dannbott101@gmail.com>
2025-12-08 13:05:21 -0700

    creation

--- CodingKatas/Kata2/obj/Debug/net9.0/Kata2.AssemblyInfo.cs
+++ CodingKatas/Kata2/obj/Debug/net9.0/Kata2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kata2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db300646ba894d109990f7cd92028615ac2ad882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc03e38b7941d0e66b5cc5971df4f019a018e892")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kata2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kata2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
